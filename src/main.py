2362eac9a6e4d0e79bc6a648f633f8853c805094
Markus Lång <markus.lang@aalto.fi>
2024-04-04 23:19:54 +0300

    removed setup.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout,
 from PyQt5.QtCore import QTimer, Qt
 from data_feeder import DataFeeder
 from trader import Trader
-from setup import *
 from plot_canvas import MarketPlotCanvas
 
 
